e62df60e9df4dc3a386f3444262c65a67aa0d069
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-04-17 11:47:35 +0400

    Functions

---
+++ 5 - Functions/assets/js/main.js
@@ -0,0 +1,69 @@
+// function sayHello(name){
+//     console.log("Salam "+name);
+// }
+// sayHello("Pakize");
+// sayHello("Nubar");
+// sayHello(prompt())
+
+// function math(x,y){
+//     console.log(Number(x)+Number(y));
+// }
+
+// math(prompt(), prompt()); // 5 * 5 * 5
+
+// function power(x,y){
+//     var result = 1;
+//     for (let i = 1; i <= y; i++) {
+//         result = result * x;
+//     }
+//     return result
+//     // console.log();
+// }
+
+
+
+// var a = power(prompt("Eded:"), prompt("Quvvet:"));
+// alert(a);
+// console.log(a);
+// console.log(a*10);
+
+
+// function emsalHesabla(isTecrubesi){
+//     return isTecrubesi / Math.floor(Math.random()*100/5)
+// }
+
+// // var exp = emsalHesabla(prompt("Is tecrubenizi daxil edin"));
+
+// // console.log("Sizin endirim %-i = "+ exp*8);
+
+// var salam1 = function (){
+//     console.log("Hello");
+// }
+
+// var salam = () => {
+//     console.log("Hello");
+// }
+// salam();
+// salam1;
+// // salam();
+// // salam();
+
+// var calc = (x,y) => x/y;
+// // var calcAnonym = function(x,y){
+// //     return x/y;
+// // }
+// alert(calc(20,1));
+
+// var a = "1 Salam 44";
+// for (let i = 0; i < a.length; i++) {
+    
+// }
+
+function hasNum(value){
+    for (let i = 0; i < value.length; i++) {
+        console.log(value[i]);
+    }
+}
+hasNum("Sa1am");
+
+// Daxil edilen deyerin ichinde eded varsa bunu bildirsin
