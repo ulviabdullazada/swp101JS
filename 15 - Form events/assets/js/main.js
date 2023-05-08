cfa98507493b41cf8579294588718fda04437ad7
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-05-08 12:59:31 +0400

    Form events

---
+++ 15 - Form events/assets/js/main.js
@@ -0,0 +1,50 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+// input.addEventListener("keyup",()=>{
+//     console.log("key up " + input.value);
+// })
+// input.addEventListener("keydown",(e)=>{
+//     if (!(e.keyCode > 47 && e.keyCode < 58)) {
+//         e.preventDefault();
+//     }
+// })
+// input.addEventListener("keypress",()=>{
+//     console.log("key press " + input.value);
+// })
+
+
+
+// input.onclick = ()=>{
+//     alert("Salam")
+// }
+// input.onclick = ()=>{
+//     console.log("Salam")
+// }
+// input.addEventListener("focus",()=>{
+//     input.style.backgroundColor = "red"
+// })
+// input.addEventListener("blur",()=>{
+//     // input.style.backgroundColor = "blue"
+//     input.classList.add('ceyran');
+// })
+// input.addEventListener("click",()=>{
+//     console.log("Salam");
+// })
+// input.addEventListener("change", ()=>{
+//     console.log("deyisildi");
+// })
+// input.addEventListener("contextmenu", (e)=>{
+//     e.preventDefault();
+// })
+
+// input.addEventListener("invalid", (e)=>{
+//     e.preventDefault();
+//     console.log("Duz elemedin");
+// })
+// button.parentElement.addEventListener("submit", (e)=>{
+//     e.preventDefault();
+//     // console.log(button.parentElement);
+// })
+
+
