4a7c588451a6514a620af435bfe892f2804f6b3c
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-05-16 11:20:10 +0400

    Ajax & axios

--- 17 - Ajax, axios/assets/js/main.js
+++ 17 - Ajax, axios/assets/js/main.js
@@ -25,17 +25,30 @@
 // console.log(obj.universitet.abr);
 
 
-for (let i = 1; i < 1011; i++) {
-    $.ajax({
-        url:'https://pokeapi.co/api/v2/pokemon/'+i,
-        method:'get',
-        // success: ,
-        error: function(err){
-            console.log(err);
-        }
-    }).done(function(res){
-        $('ol').append(
-        `<li class="list-group-item">${i}<img src="${res.sprites.front_default}">${res.name}</li>`
-        )
-    });
-}
+// for (let i = 1; i < 1011; i++) {
+//     $.ajax({
+//         url:'https://pokeapi.co/api/v2/pokemon/'+i,
+//         method:'get',
+//         // success: ,
+//         error: function(err){
+//             console.log(err);
+//         }
+//     }).done(function(res){
+//         $('ol').append(
+//         `<li class="list-group-item">${i}<img src="${res.sprites.front_default}">${res.name}</li>`
+//         )
+//     });
+// }
+
+
+const list = document.querySelector(".list-group");
+
+axios.get('https://pokeapi.co/api/v2/pokemon/').then(resp => {
+
+console.log(resp.data)
+    for (let i = 0; i < resp.data.results.length; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = resp.data.results[i].name;
+        list.appendChild(li);
+    }
+});
